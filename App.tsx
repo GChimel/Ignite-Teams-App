ac6d3b9cf5ed8d3ea9ffd56cc090ed523aa7f4dd
GChimel <gustavochimel12@gmail.com>
2024-01-09 19:14:53 -0300

    fonts & fontload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from 'react-native'
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components'
 import theme from '@theme/index';
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+
 export default function App() {
+
+  // Lógica de carregamento das fontes
+  const [fonstLoad] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups/> 
+      {fonstLoad ? <Groups/> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
+    padding: 24px;
     background-color : ${({theme}) => theme.COLORS.GRAY_600} ;
-    align-items: center;
-    justify-content: center;
 `;
 
-export const Title = styled.Text`
-    color: white;
-    font-size: 32px;
-`;
