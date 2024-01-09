6590b59a28547515c395cc40ffbb0b2ae7095c64
GChimel <gustavochimel12@gmail.com>
2024-01-09 19:23:06 -0300

    status-bar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components'
-import theme from '@theme/index';
-import { Loading } from '@components/Loading';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-
+import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
+import theme from '@theme/index';
 
 export default function App() {
-
   // Lógica de carregamento das fontes
   const [fonstLoad] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
   return (
     <ThemeProvider theme={theme}>
-      {!fonstLoad ? <Groups/> : <Loading/>}
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fonstLoad ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
