8ede9c58172f438da72cb32bd866189582bf4c23
GChimel <gustavochimel12@gmail.com>
2024-01-11 19:06:36 -0300

    groupcard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -9,6 +10,9 @@ export function Groups() {
       <Highlight
         title='Turmas'
         subtitle="TESTE"
+      />
+      <GroupCard
+        title='Galera boa'
       />
     </Container>
   );
