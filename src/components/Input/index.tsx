a4b5738406acfc1933e548782be30f8148f42627
GChimel <gustavochimel12@gmail.com>
2024-01-15 19:11:51 -0300

    input component style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,16 @@
 import { Container } from './styles';
 import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components';
 
 export function Inpu({...rest}: TextInputProps) {
+
+    const {COLORS} = useTheme();
+
     return (
-        <Container {...rest}>
-            
+        <Container 
+            placeholderTextColor={COLORS.GRAY_300}
+            {...rest}>
+
         </Container>
     );
 }
